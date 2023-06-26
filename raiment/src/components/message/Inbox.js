835a56346b2b7fa4aaa0413b58342853cb04cdca
Shakhram <shakag6@gmail.com>
2023-06-26 17:07:15 -0400

    Create Inbox component to display all  message threads

--- raiment/src/components/message/Inbox.js
+++ raiment/src/components/message/Inbox.js
@@ -4,19 +4,10 @@ import Row from "react-bootstrap/Row";
 import Button from "react-bootstrap/Button";
 import { query, collection, onSnapshot, where } from "firebase/firestore";
 import { dbFirestone } from "../../firebase";
-import Message from "./Message";
-import SendMessage from "./SendMessage";
 import { useSelector } from "react-redux";
 import { selectUser } from "../../store/userSlice";
-import { useParams } from "react-router-dom";
-import { ref, get, off } from "firebase/database";
-import firebase from "firebase/compat/app";
-import "firebase/compat/database";
-import { storage } from "../../firebase";
-import Carousel from "react-bootstrap/Carousel";
-import uuid from "react-uuid";
-import ChatBox from "./ChatBox";
 import { useNavigate } from "react-router-dom";
+import "firebase/compat/database";
 
 export default function Inbox() {
   const [messageThreads, setMessageThreads] = useState([]);
@@ -68,7 +59,7 @@ export default function Inbox() {
         <div className="messages-wrapper">
           {messageThreads?.map((message) => (
             <Col>
-              <Button onClick={() => messageSeller(message.seller, message.postKey)}>
+              <Button onClick={() => messageSeller(message.seller, message.postKey)} style={{ margin: '10px' }} >
                 See convo for listing: {message.postTitle}
               </Button>
             </Col>
