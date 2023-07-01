d02b08a75c229d9d14f246d8b349bc71dfb2216d
Shakhram <shakag6@gmail.com>
2023-07-01 19:58:20 -0400

    Display message threads with images from the posts

--- raiment/src/components/message/Inbox.js
+++ raiment/src/components/message/Inbox.js
@@ -8,11 +8,28 @@ import { useSelector } from "react-redux";
 import { selectUser } from "../../store/userSlice";
 import { useNavigate } from "react-router-dom";
 import "firebase/compat/database";
+import firebase from "firebase/compat/app";
+import { ref, get, off } from "firebase/database";
+import { storage } from "../../firebase";
+import Carousel from "react-bootstrap/Carousel";
+import Card from "react-bootstrap/Card";
+import Container from "react-bootstrap/Container";
 
 export default function Inbox() {
   const [messageThreads, setMessageThreads] = useState([]);
   const user = useSelector(selectUser);
   let navigate = useNavigate();
+
+  // NEW
+  const [posts, setPosts] = useState({});
+  const [imageURLArray, setImageURLArray] = useState([]); // contains the URL of the image
+  const [imagesLinkedToPosts, setImagesLinkedToPosts] = useState({}); // used to be empty array
+
+  useEffect(() => {
+    console.log("messageThreads", messageThreads);
+    console.log("posts", posts);
+    console.log("imagesLinkedToPosts", imagesLinkedToPosts);
+  }, [messageThreads, posts, imagesLinkedToPosts]);
 
   useEffect(() => {
     const userInboxRef = collection(
@@ -46,45 +63,126 @@ export default function Inbox() {
         (a, b) => a.createdAt - b.createdAt
       );
       setMessageThreads(sortedMessages);
+      getPosts(sortedMessages);
     });
     return () => unsubscribe;
   }, []);
 
-  //   function messageSeller(seller, key) {
-  //     navigate(`/message/${seller}:${key}`);
-  //   }
-  function messageUser(message) {
-    if (message.seller === user.username) {
-        navigate(`/messageAsSeller/${message.buyer}:${message.seller}:${message.postKey}`);
-    } else {
-        navigate(`/messageAsBuyer/${message.seller}:${message.postKey}`);
+  // NEW
+  function getPosts(messages) {
+    for (let i = 0; i < messages.length; i++) {
+      console.log("index i", i);
+      console.log("going thru these messages:", messages);
+      const seller = messages[i].seller;
+      const postKey = messages[i].postKey;
+      const db = firebase.database();
+      const dataRef = ref(db, `listings/${seller}/${postKey}`);
+
+      get(dataRef)
+        .then((snapshot) => {
+          const data = snapshot.val();
+          console.log("data", data);
+          setPosts((prevPosts) => {
+            return { ...prevPosts, [postKey]: data };
+          });
+          const names = data.images.map((image) => image.name);
+          console.log("names", names);
+          getImageUrl(names, postKey);
+        })
+        .catch((error) => {
+          console.log("Error fetching data:", error);
+        });
+
+      const getImageUrl = async (names, postKey) => {
+        try {
+          for (let i = 0; i < names.length; i++) {
+            const imageRef = storage.ref().child(names[i]);
+            const url = await imageRef.getDownloadURL();
+            console.log("getting image url for", names[i]);
+            names[i] = url;
+          }
+          console.log("names urlArray", names);
+          setImagesLinkedToPosts((prevArray) => {
+            return { ...prevArray, [postKey]: names };
+          });
+        } catch (error) {
+          console.log("Error getting image URL:", error);
+        }
+      };
     }
   }
 
-  // if ur buyer, messageSeller
-  // if ur seller, messageBuyer
+  function messageUser(message) {
+    if (message.seller === user.username) {
+      navigate(
+        `/messageAsSeller/${message.buyer}:${message.seller}:${message.postKey}`
+      );
+    } else {
+      navigate(`/messageAsBuyer/${message.seller}:${message.postKey}`);
+    }
+  }
 
   return (
     <div>
-      <Row>
+      <h3>Messages</h3>
+      <style>
+        {`
+          .column {
+            margin: 0;
+            padding: 0;
+          }
+        `}
+      </style>
+      <Container>
         <div className="messages-wrapper">
           {messageThreads.length > 0 ? (
-            messageThreads.map((message) => (
-              <Col>
-                <Button
-                  //   onClick={() => messageSeller(message.seller, message.postKey)}
-                  onClick={() => messageUser(message)}
-                  style={{ margin: "10px" }}
-                >
-                  See convo for listing: {message.postTitle}
-                </Button>
-              </Col>
-            ))
+            messageThreads.map((message, index) => {
+              const key = message.postKey;
+              const carouselKey = `carousel-${key}`; // Unique key for each Carousel
+              const cardKey = `card-${key}`; // Unique key for each Card
+              return (
+                <Row>
+                  <Col className="d-flex justify-content-center">
+                    <Card style={{ width: "10rem" }} key={cardKey}>
+                      {imagesLinkedToPosts[key] && (
+                        <Carousel key={carouselKey} interval={null}>
+                          {imagesLinkedToPosts[key].map((url, index) => {
+                            const imageKey = `image-${index}-${key}`;
+                            return (
+                              <Carousel.Item key={imageKey}>
+                                <img
+                                  className="d-block w-100"
+                                  src={url}
+                                  alt=""
+                                />
+                              </Carousel.Item>
+                            );
+                          })}
+                        </Carousel>
+                      )}
+                    </Card>
+                    <Card style={{ width: "20rem" }} key={cardKey}>
+                      {posts[key] && (
+                        <Card.Body key={cardKey}>
+                          <Card.Title>{posts[key].title}</Card.Title>
+                          <Button
+                            onClick={() => messageUser(message)}
+                            style={{ margin: "10px" }}
+                          >
+                            Send message
+                          </Button>
+                        </Card.Body>
+                      )}
+                    </Card>
+                  </Col>
+                </Row>
+              );
+            })
           ) : (
             <h3>No messages</h3>
           )}
         </div>
-      </Row>
+      </Container>
     </div>
   );
 }
