ef717ff16ededb542c7bcd44d20bb5a098f77484
Shakhram <shakag6@gmail.com>
2023-07-01 20:28:33 -0400

    Create netlify.toml file

--- raiment/src/components/landing/Slideshow.js
+++ raiment/src/components/landing/Slideshow.js
@@ -14,19 +14,10 @@ export default function Slideshow() {
     <Carousel activeIndex={index} onSelect={handleSelect}>
       <Carousel.Item>
         <img className="d-block w-100" src={img1} alt="First slide" />
-        <Carousel.Caption>
-          <h3>First slide label</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-        </Carousel.Caption>
       </Carousel.Item>
 
       <Carousel.Item>
         <img className="d-block w-100" src={img2} alt="Second slide" />
-
-        <Carousel.Caption>
-          <h3>Second slide label</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </Carousel.Caption>
       </Carousel.Item>
     </Carousel>
   );
