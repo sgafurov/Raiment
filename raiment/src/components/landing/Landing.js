04546fe02429dfbc82abf4e0a1413c3335d9b2a7
Shakhram <shakhramgafurov@gmail.com>
2023-10-28 15:55:53 +0500

    Fix the css of the landing page to be more responsive on different screens

--- raiment/src/components/landing/Landing.js
+++ raiment/src/components/landing/Landing.js
@@ -3,6 +3,7 @@ import Slideshow from "./Slideshow";
 import "../../styles/landing.css";
 import sneakersImage from "../../images/person-wearing-sneakers.jpg"
 import sellerImage from "../../images/seller-sitting-down.jpg"
+import bannerImage from "../../images/fall-essentials-copy.jpg";
 
 // import firebase from "firebase/compat/app";
 // import "firebase/analytics";
@@ -18,29 +19,37 @@ export default function Landing() {
   });
   return (
     <div className="landingContainer">
-      <div>
-        <Slideshow />
+      <div className="bannerContainer">
+        <img className="bannerImage" src={bannerImage}></img>
+        {/* <Slideshow /> */}
       </div>
 
-      <div className="narrativeCard">
-        <div className="narrativeImage">
-          <img src={sneakersImage}></img>
-        </div>
-        <div className="narrativeText">
-          <h2>Discover your unique look</h2>
-          <p>Explore top-notch brands you're familiar with, as well as emerging independent labels and the creative minds behind them. No matter your taste, locate the perfect item and seller on Raiment.</p>
-        </div>
+      <div className="narrativeDiv">
+        <section>
+          <div className="narrativeCard">
+            <div className="narrativeImage">
+              <img src={sneakersImage}></img>
+            </div>
+            <div className="narrativeText">
+              <h2>Discover your unique look</h2>
+              <p>Explore top-notch brands you're familiar with, as well as emerging independent labels and the creative minds behind them. No matter your taste, locate the perfect item and seller on Raiment.</p>
+              <a href="/signup"><span type="button" class="sc-pqjAT sc-csmWhv mRPEM dqsIjj">Shop now</span></a>
+            </div>
+          </div>
+
+          <div className="narrativeCard">
+            <div className="narrativeText">
+              <h2>Pave your path</h2>
+              <p>Whether you're looking to sell a few items or establish a thriving business, we're here to offer expert advice to guide you on your journey. Getting started is easy.</p>
+              <a href="/signup"><span type="button" class="sc-pqjAT sc-csmWhv mRPEM dqsIjj">Sell now</span></a>
+            </div>
+            <div className="narrativeImage">
+              <img src={sellerImage}></img>
+            </div>
+          </div>
+        </section>
       </div>
 
-      <div className="narrativeCard">
-        <div className="narrativeText">
-          <h2>Pave your path</h2>
-          <p>Whether you're looking to sell a few items or establish a thriving business, we're here to offer expert advice to guide you on your journey. Getting started is easy.</p>
-        </div>
-        <div className="narrativeImage">
-          <img src={sellerImage}></img>
-        </div>
-      </div>
     </div>
   );
 }
