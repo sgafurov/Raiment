5203074d37d43d03a54282bd1269efa8359e89c0
Shakhram <shakag6@gmail.com>
2024-09-30 13:47:48 -0400

    organized navbar component's code better

--- raiment/src/components/NavigationBar.js
+++ raiment/src/components/NavigationBar.js
@@ -1,25 +1,20 @@
 import React, { useState } from "react";
-import Container from "react-bootstrap/Container";
-import Nav from "react-bootstrap/Nav";
-import Navbar from "react-bootstrap/Navbar";
-// import NavDropdown from "react-bootstrap/NavDropdown";
-import Form from "react-bootstrap/Form";
-import Button from "react-bootstrap/Button";
-import Logo from "../images/logo.png";
-import ProfileIcon from "../images/profile-icon.png";
-import LogoutIcon from "../images/logout-icon.png";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { auth } from "../firebase";
 import { selectUser } from "../store/userSlice";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import Logo from "../images/logo.png";
+import ProfileIcon from "../images/profile-icon.png";
+import LogoutIcon from "../images/logout-icon.png";
 import "../styles/navigationBar.css";
-import { useNavigate } from "react-router-dom";
 
 export default function NavigationBar() {
   const user = useSelector(selectUser);
   const [inputText, setInputText] = useState("");
-
-  let navigate = useNavigate();
+  const navigate = useNavigate();
 
   const handleChange = (e) => {
     e.preventDefault();
@@ -36,81 +31,57 @@ export default function NavigationBar() {
     <div className="both-navbars">
       <Navbar bg="white" expand="lg" className="top-navbar">
         <Container className="navbar-container">
-          <div>
-            <Navbar.Brand as={Link} to="/">
-              <img src={Logo} alt="Raiment" height={40} />
-            </Navbar.Brand>
-          </div>
+          <Navbar.Brand as={Link} to="/">
+            <img src={Logo} alt="Raiment" height={40} />
+          </Navbar.Brand>
 
-          <div>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-              <Nav className="me-auto">
-                <Nav.Link as={Link} to="/allproducts/Menswear">
-                  Menswear
-                </Nav.Link>
-                <Nav.Link as={Link} to="/allproducts/Womenswear">
-                  Womenswear
-                </Nav.Link>
-                <Nav.Link as={Link} to="/allproducts/Jewelry">
-                  Jewelry
-                </Nav.Link>
-              </Nav>
-            </Navbar.Collapse>
-          </div>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link as={Link} to="/allproducts/Menswear">Menswear</Nav.Link>
+              <Nav.Link as={Link} to="/allproducts/Womenswear">Womenswear</Nav.Link>
+              <Nav.Link as={Link} to="/allproducts/Jewelry">Jewelry</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
 
-          <div>
-            <form className="search-form" onSubmit={handleSubmit}>
-              <input
-                placeholder="Search for items..."
-                type="text"
-                name="q"
-                maxLength="2048"
-                className="input-box"
-                value={inputText}
-                onChange={handleChange}
-              />
-            </form>
-          </div>
+          <form className="search-form" onSubmit={handleSubmit}>
+            <input
+              placeholder="Search for items..."
+              type="text"
+              name="q"
+              maxLength="2048"
+              className="input-box"
+              value={inputText}
+              onChange={handleChange}
+            />
+          </form>
 
-          <div>
-            <Navbar id="basic-navbar-nav">
-              <Nav className="me-auto">
-                {user ? (
-                  <>
-                    <Nav.Link as={Link} to="/upload">
-                      Upload
-                    </Nav.Link>
-                    <Nav.Link as={Link} to="/user-dashboard">
-                      Dashboard
-                    </Nav.Link>
-                    <Nav.Link as={Link} to="/inbox">
-                      Inbox
-                    </Nav.Link>
-                  </>
-                ) : (
-                  <></>
-                )}
-              </Nav>
-              {user ? (
-                <div className="nav-buttons-logged-in">
-                  <Nav.Link
-                    as={Link}
-                    to="/"
-                    onClick={() => {
-                      auth.signOut();
-                    }}
-                  >
-                    <img src={LogoutIcon} alt="Log out icon" height={30} />
-                  </Nav.Link>
-                </div>
-              ) : (
-                <Nav.Link as={Link} to="/login">
-                  <img src={ProfileIcon} alt="Profile icon" height={70} />
-                </Nav.Link>
+          <Navbar id="basic-navbar-nav">
+            <Nav className="me-auto">
+              {user && (
+                <>
+                  <Nav.Link as={Link} to="/upload">Upload</Nav.Link>
+                  <Nav.Link as={Link} to="/user-dashboard">Dashboard</Nav.Link>
+                  <Nav.Link as={Link} to="/inbox">Inbox</Nav.Link>
+                </>
               )}
-            </Navbar>
-          </div>
+            </Nav>
+            {user ? (
+              <div className="nav-buttons-logged-in">
+                <Nav.Link
+                  as={Link}
+                  to="/"
+                  onClick={() => auth.signOut()}
+                >
+                  <img src={LogoutIcon} alt="Log out icon" height={30} />
+                </Nav.Link>
+              </div>
+            ) : (
+              <Nav.Link as={Link} to="/login">
+                <img src={ProfileIcon} alt="Profile icon" height={70} />
+              </Nav.Link>
+            )}
+          </Navbar>
         </Container>
       </Navbar>
     </div>
