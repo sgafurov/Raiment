fb15ee17920b318dc389009a1bab79457d13ce44
Shakhram <shakag6@gmail.com>
2024-09-30 13:49:48 -0400

    Better organized Landing code

--- raiment/src/components/landing/Landing.js
+++ raiment/src/components/landing/Landing.js
@@ -1,23 +1,14 @@
 import React from "react";
-import Slideshow from "./Slideshow";
-import "../../styles/landing.css";
-import sneakersImage from "../../images/person-wearing-sneakers.jpg"
-import sellerImage from "../../images/seller-sitting-down.jpg"
+import sellerImage from "../../images/seller-sitting-down.jpg";
 import bannerImage from "../../images/fall-essentials-copy.jpg";
-import friendsInCar from "../../images/friendsInCar.jpg"
-
-// import firebase from "firebase/compat/app";
-// import "firebase/analytics";
-// const analytics = firebase.analytics;
+import friendsInCar from "../../images/friendsInCar.jpg";
+import "../../styles/landing.css";
 
 import { getAnalytics, logEvent } from "firebase/analytics";
 
 export default function Landing() {
-  // analytics().setCurrentScreen(window.location.pathname); // sets `screen_name` parameter
-  // analytics().logEvent("screen_view");
   const analytics = getAnalytics();
-  logEvent(analytics, "screen_view", {
-  });
+  logEvent(analytics, "screen_view", {});
   return (
     <div className="landingContainer">
       <div className="bannerContainer">
@@ -33,16 +24,33 @@ export default function Landing() {
             </div>
             <div className="narrativeText">
               <h2>Discover your unique look</h2>
-              <p>Explore top-notch brands you're familiar with, as well as emerging independent labels and the creative minds behind them. No matter your taste, locate the perfect item and seller on Raiment.</p>
-              <a href="/signup"><span type="button" class="sc-pqjAT sc-csmWhv mRPEM dqsIjj">Shop now</span></a>
+              <p>
+                Explore top-notch brands you're familiar with, as well as
+                emerging independent labels and the creative minds behind them.
+                No matter your taste, locate the perfect item and seller on
+                Raiment.
+              </p>
+              <a href="/signup">
+                <span type="button" class="sc-pqjAT sc-csmWhv mRPEM dqsIjj">
+                  Shop now
+                </span>
+              </a>
             </div>
           </div>
 
           <div className="narrativeCard">
             <div className="narrativeText">
               <h2>Pave your path</h2>
-              <p>Whether you're looking to sell a few items or establish a thriving business, we're here to offer expert advice to guide you on your journey. Getting started is easy.</p>
-              <a href="/signup"><span type="button" class="sc-pqjAT sc-csmWhv mRPEM dqsIjj">Sell now</span></a>
+              <p>
+                Whether you're looking to sell a few items or establish a
+                thriving business, we're here to offer expert advice to guide
+                you on your journey. Getting started is easy.
+              </p>
+              <a href="/signup">
+                <span type="button" class="sc-pqjAT sc-csmWhv mRPEM dqsIjj">
+                  Sell now
+                </span>
+              </a>
             </div>
             <div className="narrativeImage">
               <img src={sellerImage}></img>
@@ -50,7 +58,6 @@ export default function Landing() {
           </div>
         </section>
       </div>
-
     </div>
   );
 }
